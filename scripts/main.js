e853ebd88c8be7e3a18ebf48df13fd2e276e497e
dash zou <fxfddfno@gmail.com>
2019-12-20 10:15:29 +0200

    resizing automatic canvas after load page

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,21 @@
 const canvas = document.getElementById("ufoCanves");
 canvas.width = 900;
 canvas.height = 600;
+
+
+// Canvas automatic resizing
+function resize() {
+    // Our canvas must full height of screen regardless of the resolution
+    const height = window.innerHeight - 20;
+
+    //So we need to calculate the proper scaled width that should work well with every resolution
+    const ratio = canvas.width / canvas.height;
+    const width = height * ratio;
+
+
+    canvas.style.width = width + "px";
+    canvas.style.height = height + "px";
+
+}
+
+window.addEventListener("load", resize, false)
