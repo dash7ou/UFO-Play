1c1b48d9acf6fe36e9c984bf25e27d136bfb4e58
dash zou <fxfddfno@gmail.com>
2019-12-21 14:57:28 +0200

    add some animation

--- scripts/positionTransfer.js
+++ scripts/positionTransfer.js
@@ -1,19 +1,24 @@
 class TransferPosition {
     constructor(level) {
         this.level = level;
-        this.num = 1;
+        this.fontSize = 140;
+        this.fontColor = 255;
     }
     update(play) {
-        this.num++;
-        if (this.num > 120) { //2s 60 FPS -> 120 = 2
-            play.goToPosition(new InGamePosition())
+        this.fontSize -= 1;
+        this.fontColor -= 1.5;
+
+        if (this.fontSize < 1) {
+            // if the condition is met it will go to the InGamePosition
+            play.goToPosition(new InGamePosition(play.setting, this.level))
         }
+
     }
     draw(play) {
         ctx.clearRect(0, 0, play.width, play.height);
-        ctx.font = "80px Comic Sans MS";
+        ctx.font = this.fontSize + "px Comic Sans MS";
         ctx.textAlign = "center";
-        ctx.fillStyle = "rgba(255,255,25,1)";
+        ctx.fillStyle = "rgba(255, " + this.fontColor + ", " + this.fontColor + ",1)";
         ctx.fillText("Get Ready for level " + this.level, play.width / 2, play.height / 2);
     }
 }
